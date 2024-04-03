073c3c7ae3e4cbd3292e7ec54de1deba2e44a9c9
Thiagoferlima <th1gox2003@gmail.com>
2024-04-02 22:26:27 -0300

    Db context implementado

---
+++ PassIn/PassIn.Api/Controllers/EventsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PassIn.Application.UseCases.Events.Register;
+using PassIn.Communication.Requests;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+
+namespace PassIn.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredEventJson), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        public IActionResult Register([FromBody] RequestEventJson request) 
+        {
+            try 
+            {
+                var useCase = new RegisterEventUseCase();
+
+                useCase.Execute(request);
+
+                return Created();
+            }
+            catch (PassInException ex) 
+            {
+                return BadRequest(new ResponseErrorJson(ex.Message));
+            }
+
+            catch 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorJson("Unknown error"));
+            }
+        }
+
+    }
+}
---
+++ PassIn/PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassIn.Communication.Requests;
+using PassIn.Exceptions;
+using PassIn.Infrastructure;
+using System.Data;
+
+namespace PassIn.Application.UseCases.Events.Register
+{
+    public class RegisterEventUseCase
+    {
+        public void Execute(RequestEventJson request) 
+        {
+            Validate(request);
+            var DbContext = new PassInDbContext();
+
+            var entity = new Infrastructure.Entities.Event
+            {
+                Title = request.Title,
+                Details = request.Details,
+                Maximum_Attendees = request.MaximumAttendees,
+                Slug = request.Title.ToLower().Replace(" ", "-"),
+            };
+            DbContext.Events.Add(entity);
+            DbContext.SaveChanges();
+        }
+        private void Validate(RequestEventJson request) 
+        {
+            if (request.MaximumAttendees <= 0) 
+            {
+                throw new PassInException("The Maximum attends is invalid.");
+            }
+            if(string.IsNullOrWhiteSpace(request.Title)) 
+            {
+                throw new PassInException("the title is invalid.");
+            }
+            if (string.IsNullOrWhiteSpace(request.Details))
+            {
+                throw new PassInException("the details is invalid.");
+            }
+        }
+    }
+}
